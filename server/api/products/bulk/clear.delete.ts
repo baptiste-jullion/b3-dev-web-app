eb489499fe2379e791a77df86ef5e71b27e8a234
Baptiste <contact@baptiste-jullion.dev>
2025-05-21 15:46:51 +0200

    feat: add bulk clear and generate API handlers for product management

---
+++ server/api/products/bulk/clear.delete.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler(async () => {
+    await db.clear();
+
+    // Add a delay to simulate a real API call
+    await new Promise((resolve) =>
+        setTimeout(resolve, useFaker.number.int({ min: 500, max: 2000 })),
+    );
+
+    return;
+});
---
+++ server/api/products/bulk/generate.post.ts
@@ -0,0 +1,32 @@
+export default defineEventHandler(async () => {
+    const count = 200;
+
+    const { commerce, number, image, string } = useFaker;
+
+    const products: ProductRead[] = Array.from({ length: count }, () => {
+        const id = string.uuid();
+        return {
+            category: commerce.department(),
+            description: commerce.productDescription(),
+            image: image.url(),
+            name: commerce.productName(),
+            price: Number(commerce.price()),
+            rating: number.float({ min: 0, max: 5, fractionDigits: 1 }),
+            id,
+        };
+    });
+
+    await db.setItems(
+        products.map((product) => ({
+            key: `products/${product.id}`,
+            value: product,
+        })),
+    );
+
+    // Add a delay to simulate a real API call
+    await new Promise((resolve) =>
+        setTimeout(resolve, number.int({ min: 500, max: 2000 })),
+    );
+
+    return;
+});
