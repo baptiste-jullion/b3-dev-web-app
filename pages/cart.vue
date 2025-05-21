078a73e93cd1a7ea1c50246bdd4428ec0a2a4e33
Baptiste <contact@baptiste-jullion.dev>
2025-05-21 16:23:18 +0200

    feat: add cart component for displaying items and managing cart actions

---
+++ pages/cart.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul v-if="Object.keys(cart).length">
+        <li
+            v-for="item of cart"
+            :key="item.id"
+            class="flex items-center justify-between p-4 border-b border-neutral-300"
+        >
+            {{ item.name }}
+
+            <UButton
+                icon="i-lucide-trash-2"
+                variant="soft"
+                color="error"
+                @click="removeFromCart(item)"
+            >
+                Remove from cart
+            </UButton>
+        </li>
+    </ul>
+    <section
+        v-else
+        class="p-8 h-96 flex flex-col items-center justify-center rounded-lg border border-neutral-300 border-dashed mt-8 text-center"
+    >
+        <p class="text-xl">No products in cart</p>
+        <p>
+            You can add products to the cart from the
+            <UButton variant="soft" to="/">product list</UButton>
+        </p>
+    </section>
+</template>
+
+<script setup lang="ts">
+const { cart, removeFromCart } = useCart();
+</script>
