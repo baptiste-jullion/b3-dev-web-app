697bbce74d6f9d6c5202e40def855c17cdfae128
Baptiste <contact@baptiste-jullion.dev>
2025-05-21 16:23:11 +0200

    feat: add ProductCard component for displaying product details and cart actions

---
+++ components/ProductCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <UCard>
+        <template #header>
+            <p>{{ product.name }}</p>
+        </template>
+
+        <img
+            :src="product.image"
+            alt=""
+            lazy
+            class="object-cover aspect-square rounded-lg"
+        />
+
+        <template #footer>
+            <UButton
+                v-if="!isInCart(product)"
+                icon="i-lucide-shopping-cart"
+                @click="addToCart(product)"
+            >
+                Add to cart
+            </UButton>
+            <UButton
+                v-else
+                icon="i-lucide-trash-2"
+                variant="soft"
+                color="error"
+                @click="removeFromCart(product)"
+            >
+                Remove from cart
+            </UButton>
+        </template>
+    </UCard>
+</template>
+
+<script setup lang="ts">
+const { addToCart, removeFromCart, isInCart } = useCart();
+
+defineProps<{
+    product: ProductRead;
+}>();
+</script>
